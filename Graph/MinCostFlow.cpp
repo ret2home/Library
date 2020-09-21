4deaaef11c19e420b1097ff9c9ea98b3c280a782
define <defineprogram@gmail.com>
2020-09-21 11:00:19 +0900

    modified Min Cost Flow : speed up and available with the negative edge

--- Graph/MinCostFlow.cpp
+++ Graph/MinCostFlow.cpp
@@ -6,32 +6,58 @@ struct PrimalDual{
 		int to,cap,cost,rev;
 	};
 	vector<vector<edge>>graph;
+	bool negative=false;
 	void add_edge(int from,int to,int cap,int cost){
 		graph[from].push_back({to,cap,cost,len(graph[to])});
 		graph[to].push_back({from,0ll,-cost,len(graph[from])-1});
+		if(cost<0)negative=true;
 	}
 	int minCostFlow(int s,int t,int f){
 		int V=len(graph);
 		vector<int>potential(V),minCost,prevv(V,-1),preve(V,-1);
-		vector<bool>used(V);
 		int res=0;
-		while(f>0){
+		if(negative){
 			minCost.assign(V,inf);
-			used.assign(V,0);
 			minCost[s]=0;
 			rep(_,V-1){
-				int mn=inf,idx;
-				rep(i,V)if(!used[i]&&chmin(mn,minCost[i]))idx=i;
-				used[idx]=true;
-				rep(i,len(graph[idx])){
-					edge &e=graph[idx][i];
-					int nextcost=mn+e.cost+potential[idx]-potential[e.to];
-					if(e.cap>0&&minCost[e.to]>nextcost){
-						minCost[e.to]=nextcost;
-						prevv[e.to]=idx;preve[e.to]=i;
+				rep(i,V){
+					rep(j,len(graph[i])){
+						edge &e=graph[i][j];
+						if(e.cap>0&&chmin(minCost[e.to],minCost[i]+e.cost+potential[i]-potential[e.to])){
+							prevv[e.to]=i;
+							preve[e.to]=j;
+						}
 					}
 				}
 			}
+			if(minCost[t]==inf)return -1;
+			rep(i,V)potential[i]+=minCost[i];
+			for(int i=t;i!=s;i=prevv[i]){
+				graph[prevv[i]][preve[i]].cap--;
+				graph[i][graph[prevv[i]][preve[i]].rev].cap++;
+			}
+			res+=potential[t];
+			f--;
+		}
+
+		while(f>0){
+			minCost.assign(V,inf);
+			minCost[s]=0;
+			priority_queue<P,vector<P>,greater<>>que;
+			que.push({0,s});
+			while(!que.empty()){
+				P p=que.top();que.pop();
+				if(minCost[p.second]<p.first)continue;
+				rep(i,len(graph[p.second])){
+					edge &e=graph[p.second][i];
+					if(e.cap>0&&chmin(minCost[e.to],p.first+e.cost+potential[p.second]-potential[e.to])){
+						prevv[e.to]=p.second;
+						preve[e.to]=i;
+						que.push({minCost[e.to],e.to});
+					}
+				}
+			}
+
 			if(minCost[t]==inf)return -1;
 			rep(i,V)potential[i]+=minCost[i];
 			int addflow=f;
