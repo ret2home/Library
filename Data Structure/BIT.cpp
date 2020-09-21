08331757bc94c8f5fc6e4889df48085049264d27
define <defineprogram@gmail.com>
2020-09-21 13:43:07 +0900

    modifed BIT

--- Data Structure/BIT.cpp
+++ Data Structure/BIT.cpp
@@ -1,23 +1,24 @@
 #pragma once
 #include "../template/template.cpp"
 
+template<class T>
 class BIT{
 	int N;
-	vector<int>bit;
-	void add_(int x,int y){
+	vector<T>bit;
+	void add_(int x,T y){
 		while(x<=N){
 			bit[x]+=y;x+=x&-x;
 		}
 	}
-	int sum_(int x){
-		int res=0;
+	T sum_(int x){
+		T res=0;
 		while(x>0){
 			res+=bit[x];x-=x&-x;
 		}
 		return res;
 	}
 public:
-	int lower_bound(int w){
+	int lower_bound(T w){
 		if(w<=0)return -1;
 		int x=0;
 		int k=1;while(k*2<=N)k*=2;
@@ -29,7 +30,7 @@ public:
 		}
 		return x;
 	}
-	void add(int x,int y){add_(x+1,y);}
-	int sum(int l,int r){return sum_(r)-sum_(l);}
+	void add(int x,T y){add_(x+1,y);}
+	T sum(int l,int r){return sum_(r)-sum_(l);}
 	BIT(int x):N(x),bit(x+1){}
 };
