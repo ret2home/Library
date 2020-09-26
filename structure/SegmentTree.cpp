fe80ec575f72d4531f61ff4c266772afec404877
define <defineprogram@gmail.com>
2020-09-26 22:10:53 +0900

    modified Segment Tree

--- structure/SegmentTree.cpp
+++ structure/SegmentTree.cpp
@@ -4,7 +4,9 @@
 template<typename Monoid,typename OperatorMonoid,typename F,typename G,typename H>
 class Segtree{
 	using size_type=int32_t;
+public:
 	size_type size=1;
+private:
 	vector<Monoid>dat;
 	vector<OperatorMonoid>lazy;
 	const F f;
@@ -13,20 +15,12 @@ class Segtree{
 	Monoid M;
 	OperatorMonoid OM;
 public:
-	void set(size_type a,Monoid x){
-		dat[a+size-1]=x;
-	}
-	void init(){
-		for(size_type i=size-2;i>=0;i--){
-			dat[i]=f(dat[i*2+1],dat[i*2+2]);
-		}
-	}
 	void eval(size_type k,size_type l,size_type r){
 		if(lazy[k]!=OM){
-			dat[k]=g(dat[k],lazy[k],(r-l));
+			dat[k]=g(dat[k],lazy[k],r-l);
 			if(r-l>1){
-				lazy[2*k+1]=h(lazy[2*k+1],lazy[k]);
-				lazy[2*k+2]=h(lazy[2*k+2],lazy[k]);
+				lazy[2*k+1]=h(lazy[2*k+1],lazy[k],r-l);
+				lazy[2*k+2]=h(lazy[2*k+2],lazy[k],r-l);
 			}
 			lazy[k]=OM;
 		}
@@ -36,13 +30,13 @@ public:
 		eval(k,l,r);
 		if(r<=a||b<=l)return;
 		if(a<=l&&r<=b){
-			lazy[k]=h(lazy[k],M);
+			lazy[k]=h(lazy[k],M,r-l);
 			eval(k,l,r);
 			return;
 		}
 		update(a,b,M,k*2+1,l,(l+r)/2);
 		update(a,b,M,k*2+2,(l+r)/2,r);
-		dat[k]=f(dat[k*2+1],dat[k*2+2]);
+		dat[k]=f(dat[k*2+1],dat[k*2+2],r-l);
 	}
 	Monoid query(size_type a,size_type b,size_type k=0,size_type l=0,size_type r=-1){
 		if(r==-1)r=size;
@@ -51,7 +45,7 @@ public:
 		if(a<=l&&r<=b)return dat[k];
 		Monoid lv=query(a,b,k*2+1,l,(l+r)/2);
 		Monoid rv=query(a,b,k*2+2,(l+r)/2,r);
-		return f(lv,rv);
+		return f(lv,rv,r-l);
 	}
 	template<class C>
 	size_type minLeft(size_type a,size_type b,C &check,Monoid x,size_type k=0,size_type l=0,size_type r=-1){
--- test/SegmentTree.test.cpp
+++ test/SegmentTree.test.cpp
@@ -8,9 +8,9 @@ constexpr int mod=998244353;
 using modint=mint<mod>;
  
 using PM=pair<modint,modint>;
-auto f=[](modint a,modint b)->modint{return a+b;};
+auto f=[](modint a,modint b,int sz)->modint{return a+b;};
 auto g=[](modint a,PM b,int sz)->modint{return a*b.first+b.second*modint(sz);};
-auto h=[](PM a,PM b)->PM{return PM(a.first*b.first,a.second*b.first+b.second);};
+auto h=[](PM a,PM b,int sz)->PM{return PM(a.first*b.first,a.second*b.first+b.second);};
  
 int main(){
 	cin.tie(0);ios::sync_with_stdio(false);
@@ -19,9 +19,8 @@ int main(){
 	Segtree<modint,PM,decltype(f),decltype(g),decltype(h)>segtree(N,f,g,h,0,PM(1,0));
 	rep(i,N){
 		int a;cin>>a;
-		segtree.set(i,a);
+		segtree.update(i,i+1,PM(1,a));
 	}
-	segtree.init();
 	while(Q--){
 		int t;cin>>t;
 		if(!t){
