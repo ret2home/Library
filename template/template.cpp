02292338552dbd8e223ea66d15505178b05faba4
define <defineprogram@gmail.com>
2021-09-12 23:44:25 +0900

    やっぱ bits/stdc++.h 使う

--- template/template.cpp
+++ template/template.cpp
@@ -1,56 +1,5 @@
 #pragma once
-#include <algorithm>
-#include <bitset>
-#include <complex>
-#include <deque>
-#include <exception>
-#include <fstream>
-#include <functional>
-#include <iomanip>
-#include <ios>
-#include <iosfwd>
-#include <iostream>
-#include <istream>
-#include <iterator>
-#include <limits>
-#include <list>
-#include <locale>
-#include <map>
-#include <memory>
-#include <new>
-#include <numeric>
-#include <ostream>
-#include <queue>
-#include <set>
-#include <sstream>
-#include <stack>
-#include <stdexcept>
-#include <streambuf>
-#include <string>
-#include <typeinfo>
-#include <utility>
-#include <valarray>
-#include <vector>
-#include <array>
-#include <atomic>
-#include <chrono>
-#include <codecvt>
-#include <condition_variable>
-#include <forward_list>
-#include <future>
-#include <initializer_list>
-#include <mutex>
-#include <random>
-#include <ratio>
-#include <regex>
-#include <scoped_allocator>
-#include <system_error>
-#include <thread>
-#include <tuple>
-#include <typeindex>
-#include <type_traits>
-#include <unordered_map>
-#include <unordered_set>
+#include <bits/stdc++.h>
 using namespace std;
 #define ll long long
 #define rep(i, n) for (int i = 0; i < n; i++)
@@ -59,10 +8,16 @@ using namespace std;
 #define REV(i, n) for (int i = n - 1; i > 0; i--)
 #define all(v) v.begin(), v.end()
 #define PL pair<ll, ll>
-#define PI pair<int,int>
+#define PI pair<int, int>
+#define pi acos(-1)
 #define len(s) (int)s.size()
-#define compress(v) sort(all(v)); v.erase(unique(all(v)),v.end());
-#define comid(v,x) lower_bound(all(v),x)-v.begin()
+#define compress(v) \
+    sort(all(v));   \
+    v.erase(unique(all(v)), v.end());
+#define comid(v, x) lower_bound(all(v), x) - v.begin()
+
+template<class T>
+using prique=priority_queue<T,vector<T>,greater<>>;
 
 template <class T, class U>
 inline bool chmin(T &a, U b) {
